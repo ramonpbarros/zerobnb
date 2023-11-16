3b9c6a9ad3fd7df5480f3e30d72e2880f22bf0a5
Ramon <ramonpbarros@gmail.com>
2023-11-15 23:15:24 -0800

    seeders refactored to add firstname and lastname attributes

--- backend/db/seeders/20231114042319-demo-user.js
+++ backend/db/seeders/20231114042319-demo-user.js
@@ -13,16 +13,22 @@ module.exports = {
     await User.bulkCreate([
       {
         email: 'demo@user.io',
+        firstName: 'demonName1',
+        lastName: 'demonLast1',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
         email: 'user1@user.io',
+        firstName: 'demonName2',
+        lastName: 'demonLast2',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
         email: 'user2@user.io',
+        firstName: 'demonName2',
+        lastName: 'demonLast2',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
       }
