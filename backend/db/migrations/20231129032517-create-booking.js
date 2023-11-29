00c4893fc0cc2b08e76248c02b0efca79296a73f
Ramon <ramonpbarros@gmail.com>
2023-11-29 12:27:36 -0800

    fixed error with undoing seeds and migrations

--- backend/db/migrations/20231129032517-create-booking.js
+++ backend/db/migrations/20231129032517-create-booking.js
@@ -52,6 +52,6 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     options.tableName = 'Bookings';
-    return queryInterface.dropTable('options');
+    return queryInterface.dropTable(options);
   },
 };
