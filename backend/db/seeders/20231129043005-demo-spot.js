126e666437c3f4bddecc42f0939b13900050ac87
Ramon <ramonpbarros@gmail.com>
2023-11-29 12:48:32 -0800

    demo-spot modified

--- backend/db/seeders/20231129043005-demo-spot.js
+++ backend/db/seeders/20231129043005-demo-spot.js
@@ -1,56 +1,82 @@
 'use strict';
 
-const { Spot } = require('../models');
+const { Spot, User } = require('../models');
 
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;
 }
 
+const spots = [
+  {
+    user: 'Demo-lition',
+    address: '1234 Some Address',
+    city: 'City',
+    state: 'State',
+    lat: 38.8951,
+    lng: -77.0364,
+    name: 'Name1',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod libero sed ante scelerisque pharetra. Morbi sagittis risus id massa porttitor luctus',
+    price: 100,
+  },
+  {
+    user: 'FakeUser1',
+    address: '1234 Some Address',
+    city: 'City',
+    state: 'State',
+    lat: 38.8951,
+    lng: -77.0364,
+    name: 'Name2',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod libero sed ante scelerisque pharetra. Morbi sagittis risus id massa porttitor luctus',
+    price: 100,
+  },
+  {
+    user: 'FakeUser2',
+    address: '1234 Some Address',
+    city: 'City',
+    state: 'State',
+    lat: 38.8951,
+    lng: -77.0364,
+    name: 'Name3',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod libero sed ante scelerisque pharetra. Morbi sagittis risus id massa porttitor luctus',
+    price: 100,
+  },
+];
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     try {
-      await Spot.bulkCreate(
-        [
-          {
-            userId: 1,
-            address: '1234 Some Address',
-            city: 'City',
-            state: 'State',
-            lat: 38.8951,
-            lng: -77.0364,
-            name: 'Name',
-            description:
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod libero sed ante scelerisque pharetra. Morbi sagittis risus id massa porttitor luctus',
-            price: 100,
-          },
-          {
-            userId: 2,
-            address: '1234 Some Address',
-            city: 'City',
-            state: 'State',
-            lat: 38.8951,
-            lng: -77.0364,
-            name: 'Name',
-            description:
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod libero sed ante scelerisque pharetra. Morbi sagittis risus id massa porttitor luctus',
-            price: 100,
-          },
-          {
-            userId: 3,
-            address: '1234 Some Address',
-            city: 'City',
-            state: 'State',
-            lat: 38.8951,
-            lng: -77.0364,
-            name: 'Name',
-            description:
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod libero sed ante scelerisque pharetra. Morbi sagittis risus id massa porttitor luctus.',
-            price: 100,
-          },
-        ],
-        { validate: true }
-      );
+      for (let spotInfo of spots) {
+        const {
+          user,
+          address,
+          city,
+          state,
+          lat,
+          lng,
+          name,
+          description,
+          price,
+        } = spotInfo;
+        const foundUser = await User.findOne({
+          where: { username: user },
+        });
+
+        await Spot.bulkCreate({
+          userId: foundUser.id,
+          address,
+          city,
+          state,
+          lat,
+          lng,
+          name,
+          description,
+          price,
+        });
+      }
     } catch (error) {
       console.error('Error during Spot bulkCreate:', error);
       throw error;
